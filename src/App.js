f9dba746073f33a6a94fcb358c9f67609d0f5e1e
keropossoconsigoemagrecer-ui <keropossoconsigoemagrecer@gmail.com>
2026-05-29 12:05:06 -0300

    Create App component with welcome message and link

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px', fontFamily: 'sans-serif' }}>
+      <h1>Casa do Norte Tia Ritinha</h1>
+      <p>Estamos atualizando nosso site para melhor atendê-los!</p>
+      <a href="https://atlasautomacao.app.br/casadonorte" style={{ display: 'block', margin: '20px', padding: '15px', background: '#dcb265', color: 'white', textDecoration: 'none' }}>
+        Acessar nosso App de Pedidos
+      </a>
+    </div>
+  );
+}
+
+export default App;
